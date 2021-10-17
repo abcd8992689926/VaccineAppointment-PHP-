a42d32dfec4744512ee541525df625b8fdd74b87
abcd8992689926 <abcd8992689926@gmail.com>
2021-10-17 20:08:42 +0800

    modify script src

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ Test
 <link rel="stylesheet" href="style.css">
 
 </head>
-<script src="http://apps.bdimg.com/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <body>
         <?php
                 if (isset($_SESSION["login"]) && $_SESSION["login"] == true):
